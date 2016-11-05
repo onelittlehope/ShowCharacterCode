fc084277a618657a9bca1258c97f9e0ccc85b68e
Jinesh Choksi <jchoksi@gmail.com>
2016-11-05 22:44:30 +0000

    Displays the character code in byte, decimal and hexadecimal.

--- show_char_code.py
+++ show_char_code.py
@@ -11,4 +11,4 @@ class ShowCharCodeCommand(sublime_plugin.EventListener):
 
     def show_char_code(self, view):
         selected = view.substr(view.sel()[0].a)
-        view.set_status('charcode', "Char Code: {0}".format(ord(selected)))
+        view.set_status('charcode', "BYTE {0}, DEC {1:d}, HEX {2:#x}".format(selected.encode('unicode_escape'), ord(selected),ord(selected)))
